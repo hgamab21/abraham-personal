eddb271180f93394fa87e93f3c7b2fc74739ecad
Hiram Abraham <hgamab12@gmail.com>
2020-05-13 12:48:52 -0500

    Added bg image, app-wide styling via App.css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "../../styles/About.css";
 
 function About() {
   return (
-    <div>
-      <h1>About Me.</h1>
-      <p>
+    <div className="about-main">
+      <h1 className="title">About Me.</h1>
+      <p className="info">
       Filler content.
+      </p>
+      <p>
       </p>
     </div>
   );
